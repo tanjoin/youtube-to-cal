06ee3f9e5bc80e59084948041fca34db27b4286d
tanjoin <tanjoin@users.noreply.github.com>
2021-10-13 14:53:43 +0900

    修正

--- src/action.js
+++ src/action.js
@@ -1,13 +1,25 @@
 function openCal() {
   const BASE_URL = "https://www.google.com/calendar/render?action=TEMPLATE&text=";
+  const AND_LOCATION = "&location=";
+  const AND_DATES = "&dates=";
+  const ESCAPE_SLASH = "%2F";
+  const QUERY_DATE = '#movie_player div.ytp-offline-slate-subtitle-text';
   const NL = "\n";
   let TEXT = "";
-  if (document.querySelector('.title')) {
-    TEXT = document.querySelector('.title').textContent + NL;
+  let titles = [...document.querySelectorAll('h1.title')]
+      .map((e) => e.textContent.trim())
+      .filter((e) => e.length > 0);
+  if (titles.length > 0) {
+    TEXT = titles.pop() + NL;
   }
   const YOUTUBE_URL = window.location.href;
-  const DATE = new Date(new Date().getFullYear() + "/" + document.querySelector('#movie_player > div.ytp-offline-slate.ytp-offline-slate-collapsed > div.ytp-offline-slate-bar > span.ytp-offline-slate-messages > div.ytp-offline-slate-subtitle-text').textContent.replace('月','/').replace('日','')).toISOString().replaceAll(/[/.:-]/g, '');
-  var url = BASE_URL + encodeURIComponent(TEXT) + "&location=" + encodeURIComponent(YOUTUBE_URL) + "&dates=" + DATE + "%2F" + DATE;
+  const DATE = new Date(new Date().getFullYear() + "/" + document.querySelector(QUERY_DATE).textContent.replace('月','/').replace('日',''))
+               .toISOString()
+               .replaceAll(/[/.:-]/g, '');
+  var url = BASE_URL + encodeURIComponent(TEXT) + 
+            AND_LOCATION + encodeURIComponent(YOUTUBE_URL) + 
+            AND_DATES + DATE + 
+            ESCAPE_SLASH + DATE;
   open(url, "_blank");
 }
 
